38dfd2d664243063880c8f94fabd3145de6ffdfe
Luke Hoffmann <luke.hoffmann@hey.com>
2025-08-15 17:02:23 +0930

    Actually train and compile models

--- improver/calibration/rainforest_training.py
+++ improver/calibration/rainforest_training.py
@@ -1,11 +1,12 @@
+import os
 
 from improver.calibration import (
-    treelite_packages_available,
     lightgbm_package_available,
+    treelite_packages_available,
 )
 
 
-class TrainRainForestsCalibration():
+class TrainRainForestsCalibration:
     # pass
     def __new__(cls, lead_times, thresholds, output_path):
         """Initialise class object based on package availability."""
@@ -20,25 +21,22 @@ class TrainRainForestsCalibration():
             cls = TrainRainForestsCalibrationLightGBM
         return super(TrainRainForestsCalibration, cls).__new__(cls)
 
-
     def __init__(self, lead_times, thresholds, output_path):
-        self.lead_times = list( int(l) for l in lead_times )
-        self.thresholds = list( float(t) for t in thresholds )
+        self.lead_times = list(int(l) for l in lead_times)
+        self.thresholds = list(float(t) for t in thresholds)
         self.output_path = output_path
 
         self.config = {
-            self.lead_time_key(l) : {
-                self.threshold_key(t): {} for t in self.thresholds
-            } for l in self.lead_times
+            self.lead_time_key(l): {self.threshold_key(t): {} for t in self.thresholds}
+            for l in self.lead_times
         }
 
-
-    def process(self) -> None:
+    def process(self, training_data, obs_column, train_columns) -> None:
         """Subclasses should override this function."""
         raise NotImplementedError(
             "Process function must be called via subclass method."
         )
-
+        return self.config
 
     def lead_time_key(self, lead_time):
         return f"{lead_time:02d}"
@@ -49,9 +47,8 @@ class TrainRainForestsCalibration():
     def model_filename(self, lead_time, threshold, extension):
         return f"{lead_time:03d}H_{threshold:06.4f}.{extension}"
 
+
 class TrainRainForestsCalibrationLightGBM(TrainRainForestsCalibration):
-
-
     def __new__(cls, lead_times, thresholds, output_path):
         return super(TrainRainForestsCalibration, cls).__new__(cls)
 
@@ -63,26 +60,28 @@ class TrainRainForestsCalibrationLightGBM(TrainRainForestsCalibration):
         "seed": 0,
     }
 
-
-    def process(
-        self, training_data, obs_column, train_columns
-    ):
+    def process(self, training_data, obs_column, train_columns):
         """Train a model for each threshold."""
         output_path = self.output_path / "lightgbm_models"
+        os.makedirs(output_path, exist_ok=True)
 
         for lead_time in self.lead_times:
             lkey = self.lead_time_key(lead_time)
             for threshold in self.thresholds:
                 tkey = self.threshold_key(threshold)
-                model = self._train_model(training_data, obs_column, train_columns, lead_time, threshold)
-                # TODO: Save model to the path
-                path = str(output_path / self.model_filename(lead_time, threshold, "txt"))
-                self.config[lkey][tkey]['lightgbm_model'] = path
+
+                filepath = output_path / self.model_filename(lead_time, threshold, "txt")
+                model = self._train_model(
+                    training_data, obs_column, train_columns, lead_time, threshold
+                )
+                model.save_model(filepath)
+                self.config[lkey][tkey]["lightgbm_model"] = str(filepath)
 
         return self.config
 
-
-    def _train_model(self, training_data, obs_column, train_columns, lead_time, threshold):
+    def _train_model(
+        self, training_data, obs_column, train_columns, lead_time, threshold
+    ):
         """Train a model for each threshold."""
         import lightgbm
 
@@ -98,50 +97,53 @@ class TrainRainForestsCalibrationLightGBM(TrainRainForestsCalibration):
         return lightgbm.train(self.lightgbm_params, data)
 
 
-class TrainRainForestsCalibrationTreelite(TrainRainForestsCalibrationLightGBM):
+    def _get_model(self, lead_time, threshold):
+        import lightgbm
 
+        lkey = self.lead_time_key(lead_time)
+        tkey = self.threshold_key(threshold)
+        lightgbm_model_filepath = self.config[lkey][tkey]['lightgbm_model']
+        return lightgbm.Booster(model_file=lightgbm_model_filepath)
+
+class TrainRainForestsCalibrationTreelite(TrainRainForestsCalibrationLightGBM):
     treelight_params = {"parallel_comp": 8, "quantize": 1}
 
-    def __new__(cls, lead_times, thresholds, output_path):
+    def __new__(cls):
         return super(TrainRainForestsCalibration, cls).__new__(cls)
 
-
     def process(self, training_data, obs_column, train_columns):
-        TrainRainForestsCalibrationLightGBM.process(self, training_data, obs_column, train_columns)
+        TrainRainForestsCalibrationLightGBM.process(
+            self, training_data, obs_column, train_columns
+        )
+        import lightgbm
 
         output_path = self.output_path / "treelite_models"
+        os.makedirs(output_path, exist_ok=True)
+
         for lead_time in self.lead_times:
             lkey = self.lead_time_key(lead_time)
             for threshold in self.thresholds:
                 tkey = self.threshold_key(threshold)
-                # TODO: Get lightgbm model from the path
-                # lightgbm_model = self.config[lkey][tkey]['lightgbm_model']
 
-                # TODO: Compile and save model to the path
-                # treelite_model = self._compile_model(lightgbm_model, path)
-
-                path = str(output_path / self.model_filename(lead_time, threshold, "so"))
-                self.config[lkey][tkey]['treelite_model'] = path
+                lightgbm_model = self._get_model(lead_time, threshold)
+                path = output_path / self.model_filename(lead_time, threshold, "so")
+                self._compile_model(lightgbm_model, path)
+                self.config[lkey][tkey]["treelite_model"] = str(path)
 
         return self.config
 
-
-    def _compile_model(self, lightgbm_model, path):
+    def _compile_model(self, lightgbm_model, filepath):
         """Compile a lightgbm model."""
         import tl2cgen
         import treelite
 
-        libpath = str(path / "model.so")
         treelite_model = treelite.Model.from_lightgbm(lightgbm_model)
         tl2cgen.export_lib(
             treelite_model,
             toolchain="gcc",
-            libpath=libpath,
+            libpath=filepath,
             verbose=False,
             params=self.treelight_params,
         )
-        predictor = tl2cgen.Predictor(
-            libpath, verbose=True, nthread=1
-        )
+        predictor = tl2cgen.Predictor(filepath, verbose=True, nthread=1)
         return predictor
-
--- improver_tests/calibration/rainforests_training/test_TrainRainForestsCalibration.py
+++ improver_tests/calibration/rainforests_training/test_TrainRainForestsCalibration.py
@@ -1,12 +1,11 @@
-
+from typing import Dict
 
 import pytest
-from typing import Dict
 
 from improver.calibration.rainforest_training import (
     TrainRainForestsCalibration,
-    TrainRainForestsCalibrationLightGBM,
 )
+
 
 @pytest.mark.parametrize("available", [True, False])
 def test__new__(treelite_available):
@@ -25,41 +24,29 @@ def test__new__(treelite_available):
 def test__init__(treelite_available, tmp_path):
     """Test class is created with lead times and thresholds."""
 
-
-    result = TrainRainForestsCalibration([ 24, 12 ], [ 0.1, 0.05, 0.01 ], tmp_path)
-    assert result.lead_times == [ "24", "12" ]
-    assert result.thresholds == [ "0.1", "0.0500", "0.0100" ]
-
-
-@pytest.mark.parametrize("available", [True, False])
-def test__init__(treelite_available, tmp_path):
-    """Test class is initialised with config structure based on lead times and thresholds."""
-
-    result = TrainRainForestsCalibration([ 36, 24, 12 ], [ 0.1, 0.01 ], tmp_path)
-
-    # Config is initialised as a nested set of dictionaries
-    assert result.config == {
-        "36": { "0.1000": {}, "0.0100": {} },
-        "24": { "0.1000": {}, "0.0100": {} },
-        "12": { "0.1000": {}, "0.0100": {} }
-    }
+    result = TrainRainForestsCalibration([24, 12], [0.1, 0.05, 0.01], tmp_path)
+    assert result.lead_times == [24, 12]
+    assert result.thresholds == [0.1, 0.0500, 0.0100]
 
 
 @pytest.mark.parametrize("available", [True, False])
 def test__init__config(treelite_available, tmp_path):
     """Test class is initialised with config structure based on lead times and thresholds."""
 
-    result = TrainRainForestsCalibration([ 24, 12 ], [ 0.1, 0.01 ], tmp_path)
+    result = TrainRainForestsCalibration([36, 24, 12], [0.1, 0.01], tmp_path)
 
     # Config is initialised as a nested set of dictionaries
     assert result.config == {
-        "24": { "0.1000": {}, "0.0100": {} },
-        "12": { "0.1000": {}, "0.0100": {} }
+        "36": {"0.1000": {}, "0.0100": {}},
+        "24": {"0.1000": {}, "0.0100": {}},
+        "12": {"0.1000": {}, "0.0100": {}},
     }
 
 
 @pytest.mark.parametrize("available", [True, False])
-def test_process_lightgbm(treelite_available, lead_times, thresholds, deterministic_training_data, tmp_path):
+def test_process_lightgbm(
+    treelite_available, lead_times, thresholds, deterministic_training_data, tmp_path
+):
     """Test lightgbm models are created."""
 
     trainer = TrainRainForestsCalibration(lead_times, thresholds, tmp_path)
@@ -72,17 +59,31 @@ def test_process_lightgbm(treelite_available, lead_times, thresholds, determinis
     assert isinstance(result, Dict)
     assert isinstance(result["24"], Dict)
     assert isinstance(result["24"]["0.0000"], Dict)
-    assert result["24"]["0.0000"]["lightgbm_model"] == f"{tmp_path}/lightgbm_models/024H_0.0000.txt"
+    assert (
+        result["24"]["0.0000"]["lightgbm_model"]
+        == f"{tmp_path}/lightgbm_models/024H_0.0000.txt"
+    )
     assert isinstance(result["24"]["0.0001"], Dict)
-    assert result["24"]["0.0001"]["lightgbm_model"] == f"{tmp_path}/lightgbm_models/024H_0.0001.txt"
+    assert (
+        result["24"]["0.0001"]["lightgbm_model"]
+        == f"{tmp_path}/lightgbm_models/024H_0.0001.txt"
+    )
     assert isinstance(result["24"]["0.0010"], Dict)
-    assert result["24"]["0.0010"]["lightgbm_model"] == f"{tmp_path}/lightgbm_models/024H_0.0010.txt"
+    assert (
+        result["24"]["0.0010"]["lightgbm_model"]
+        == f"{tmp_path}/lightgbm_models/024H_0.0010.txt"
+    )
     assert isinstance(result["24"]["0.0100"], Dict)
-    assert result["24"]["0.0100"]["lightgbm_model"] == f"{tmp_path}/lightgbm_models/024H_0.0100.txt"
+    assert (
+        result["24"]["0.0100"]["lightgbm_model"]
+        == f"{tmp_path}/lightgbm_models/024H_0.0100.txt"
+    )
 
 
-@pytest.mark.parametrize("available", [ False ])
-def test_process_treelite_unavailable(treelite_available, lead_times, thresholds, deterministic_training_data, tmp_path):
+@pytest.mark.parametrize("available", [False])
+def test_process_treelite_unavailable(
+    treelite_available, lead_times, thresholds, deterministic_training_data, tmp_path
+):
     """Test treelite models are not created when treelite is not available."""
 
     trainer = TrainRainForestsCalibration(lead_times, thresholds, tmp_path)
@@ -94,14 +95,16 @@ def test_process_treelite_unavailable(treelite_available, lead_times, thresholds
 
     # Config is initialised as a nested set of dictionaries
     # LightGBM and Treelite paths are present
-    assert result["24"]["0.0000"].get("treelite_model") == None
-    assert result["24"]["0.0010"].get("treelite_model") == None
-    assert result["24"]["0.0010"].get("treelite_model") == None
-    assert result["24"]["0.0100"].get("treelite_model") == None
+    assert result["24"]["0.0000"].get("treelite_model") is None
+    assert result["24"]["0.0010"].get("treelite_model") is None
+    assert result["24"]["0.0010"].get("treelite_model") is None
+    assert result["24"]["0.0100"].get("treelite_model") is None
 
 
-@pytest.mark.parametrize("available", [ True ])
-def test_process_treelite_available(treelite_available, lead_times, thresholds, deterministic_training_data, tmp_path):
+@pytest.mark.parametrize("available", [True])
+def test_process_treelite_available(
+    treelite_available, lead_times, thresholds, deterministic_training_data, tmp_path
+):
     """Test treelite models are created when treelite is available."""
 
     trainer = TrainRainForestsCalibration(lead_times, thresholds, tmp_path)
@@ -113,10 +116,19 @@ def test_process_treelite_available(treelite_available, lead_times, thresholds, 
 
     # Config is initialised as a nested set of dictionaries
     # LightGBM and Treelite paths are present
-    assert result["24"]["0.0000"]["treelite_model"] == f"{tmp_path}/treelite_models/024H_0.0000.so"
-    assert result["24"]["0.0001"]["treelite_model"] == f"{tmp_path}/treelite_models/024H_0.0001.so"
-    assert result["24"]["0.0010"]["treelite_model"] == f"{tmp_path}/treelite_models/024H_0.0010.so"
-    assert result["24"]["0.0100"]["treelite_model"] == f"{tmp_path}/treelite_models/024H_0.0100.so"
-
-
-
+    assert (
+        result["24"]["0.0000"]["treelite_model"]
+        == f"{tmp_path}/treelite_models/024H_0.0000.so"
+    )
+    assert (
+        result["24"]["0.0001"]["treelite_model"]
+        == f"{tmp_path}/treelite_models/024H_0.0001.so"
+    )
+    assert (
+        result["24"]["0.0010"]["treelite_model"]
+        == f"{tmp_path}/treelite_models/024H_0.0010.so"
+    )
+    assert (
+        result["24"]["0.0100"]["treelite_model"]
+        == f"{tmp_path}/treelite_models/024H_0.0100.so"
+    )
